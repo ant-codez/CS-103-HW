560a0997e97ff92879390cbc167d8f381aaab955
ant-codez <ajserebii@gmail.com>
2021-03-14 16:25:26 -0700

    forgot comments

--- HW07/file_analysis.cpp
+++ HW07/file_analysis.cpp
@@ -6,10 +6,11 @@
 // Class: CSCI 123 ("Intro to Programming Using C++")
 // Time: 
 // Instructor: Dr. Rodriguez
-// Project: Cash Register Files
+// Project: File Analysis
 //
 // Description:
-// Modification of cashRegisterLoop.cpp that returns a recipt and reads input in from a file.
+// Reads the dracula.txt file and outputs the total number of periods, question marks,
+// newlines, characters, digits, punctuation marks, spaces, and upper and lower case characters
 // ============================================================================
 #include <iostream>
 #include <fstream>
@@ -31,6 +32,7 @@ int main(){
     int totalPeriods = 0, totalQuestion = 0;
     ifstream text("dracula.txt");
 
+    //stop ifstream from skipping whitespaces
     text >> std::noskipws;
     
     totalChars = ReadFile(text, totalUpper, totalLower, totalDigits, totalPunct, totalSpace, totalLines, totalPeriods, totalQuestion);
@@ -38,18 +40,33 @@ int main(){
     return (0);
 }
 
+// ==== ReadFile ==================================================================
+// Reads the ifstream from the inFile variable and returns the total number of digits in
+// the file. This Function also prints the total number of Uppercase and Lowercase Characters,
+// digits, punctutations, whitespaces, newlines, periods and question marks to the console
+//
+// Input:
+//      &inFile [IFSTREAM]   -- The file stream we will use to recieve each character from the txt
+//      totalUpper  [INT] -- The total number of uppercase characters
+//      totalLower  [INT] -- The total number of lowercase characters
+//      totalDigits  [INT] -- The total number of digits
+//      totalPunct [INT] -- The total number of punctuation characters
+//      totalSpace  [INT] -- The total number of whitespace characters
+//      totalLines  [INT] -- The total number of newline characters
+//      totalPeriods  [INT] -- The total number of period '.' characters
+//      totalQuestion  [INT] -- The total number of question mark '?' characters
+//
+// Output:
+//      Returns an integer value of the total number of charcters in the text.
+//
+// ======================================================================================
+
 int ReadFile(ifstream &inFile, int totalUpper, int totalLower, int totalDigits, int totalPunct, int totalSpace, int totalLines, int totalPeriods, int totalQuestion){
     char c;
-    string s;
     int totalChars = 0;
 
-    
-
-    
     while (inFile.get(c)){
-        //c = infile.get();
-
-        if (c == '\n' || c == '\f' || c == '\r'){
+        if (c == '\n' || c == '\f'){
             totalLines++;
         }
         else{
@@ -78,7 +95,6 @@ int ReadFile(ifstream &inFile, int totalUpper, int totalLower, int totalDigits, 
         else if (c == '?'){
             totalQuestion++;
         }
-        //cout << c << endl;
     }
 
     printf("File \"dracula.txt\" contains the following:\nUppercase letters:\t%i\n", totalUpper);
