093b014c7e5b35c13d4e74ee05795cfe7e0e3cc3
ant-codez <ajserebii@gmail.com>
2021-09-04 14:59:41 -0700

    uploading files to git

---
+++ CS133/HW02/main.cpp
@@ -0,0 +1,32 @@
+// ============================================================================
+// File: main.cpp
+// ============================================================================
+// Programmer: ???
+// Date: ???
+// Class: CSCI 133 ("Data Structures Using C++")
+// Time: ???
+// Instructor: Dr. Rodriguez
+// Project: ???
+//
+// Description:
+//      This program demonstrates the usage of advanced class composition
+//
+// ============================================================================
+
+???
+
+
+int     main()
+{
+	// Create an object
+    CMathStud   stud1(1,2);
+    CMathStud   stud2(3,4,5);
+
+    // Evaluation
+    cout << "Stud1 Eval: " << stud1.getMyValue(3.14159) << endl;
+    cout << "Stud2 Eval: " << stud2.getMyValue(3.14159) << endl << endl;
+
+    return 0;
+
+}  // end of "main"
+
