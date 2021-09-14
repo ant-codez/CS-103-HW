b06f0dbd9a5f36aa05d869be626d35e8d96b788d
ant-codez <achavez@student.42.us.org>
2021-09-13 23:07:34 -0700

    push

--- CS223-HW/HW06/isascii.c
+++ CS223-HW/HW06/isascii.c
@@ -3,26 +3,30 @@
 #include <string.h>
 #include <stdlib.h>
 
-void readBit(int c, int total){
+void readBit(int c, int total, int *p){
     int originalBit = c;
     int bitPos = 7;
 
     int bit = (c >> bitPos) & 1U;
 
     if (bit == 1){
-        printf("NON-ASCII INPUT:%X at offset 0x%i\n", originalBit, total);
+        printf("NON-ASCII INPUT: %X at offset 0x%i\n", originalBit, total);
+        p++;
     }
 }
 
 int main() {
     auto int c;
     int total = 0;
+    int bitsFound = 0;
 
     while (EOF != (c = getchar())){
-        readBit(c, total++);
+        readBit(c, total++, &bitsFound);
     }
 
-    //printf("%i", bit);
+    if (bitsFound == 0) {
+        printf("The input stream was pure ASCII with a total of %i bytes read.", total);
+    }
 
     return 0;
 }
